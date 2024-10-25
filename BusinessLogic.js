aa1fba40c6db3c01db3996d3910e9b11b6d5a10c
Callum Cunha <callum.cunha@os.uk>
2024-10-25 16:30:19 +0100

    Improved version (check previous commit for dojo's submission)

--- BusinessLogic.js
+++ BusinessLogic.js
@@ -1,49 +1,28 @@
-function pagination (currentPage, totalNumberOfPages) {
-    const output = [];
-
-    if (currentPage > 2) { 
-        output.push("1")
-    }
-
-    for(let i = currentPage - 1; i <= currentPage + 1; i++) {
-
-        output.push(i);
-    }
-    
-    if (currentPage < totalNumberOfPages -1) {
-        output.push(totalNumberOfPages);
-    }
-
-    return output.join(" ");
-}
-
-function pagination2 (currentPage, totalNumberOfPages) {
-    const importantNumbers = new Set([
-        1,
-        Math.max(1, currentPage - 1),
-        currentPage,
-        Math.min(totalNumberOfPages, currentPage + 1),
-        totalNumberOfPages
-    ]);
-
-    const output = Array.from(importantNumbers);
-
-    if (importantNumbers[1] !== 2) {
-        output.shift();
-        output.unshift([1, '...']) // we got javascripted
-    }
-
-    return output.join(" ");
-}
-
-exports.pagination = pagination;
-exports.pagination2 = pagination2;
-
 /**
- * Join example
- * 
- * const array = ["1", "2", "3"]
- * const joinedArray = array.join(" ");
- * 
- * joinedArray will be "1 2 3"
+ * Creates the pagination pages for selection to be displayed in `O(1)` time.
+ * @param {number} currentPage The current page within the pagination
+ * @param {number} totalNumberOfPages The total number of pages
+ * @returns {string} String showing correctly visible page numbers with ellipses (...) when required.
  */
+function pagination(currentPage, totalNumberOfPages) {
+  // Edgecase: for fewer than 5 pages
+  if (totalNumberOfPages < 5) return Array.from({ length: totalNumberOfPages }, (_, i) => i + 1).join(" ");
+
+  const importantNumbers = new Set([
+    1,
+    Math.max(2, currentPage - 1),
+    currentPage,
+    Math.min(totalNumberOfPages - 1, currentPage + 1),
+    totalNumberOfPages,
+  ]);
+
+  const output = Array.from(importantNumbers); 
+
+  // Add ellipses where gaps exist
+  if (output[1] > 2) output.splice(1, null, "...");
+  if (output[output.length - 2] < totalNumberOfPages - 1) output.splice(output.length - 1, null, "...");
+
+  return output.join(" ");
+}
+
+module.exports = pagination;
--- Sample.test.js
+++ Sample.test.js
@@ -1,34 +1,39 @@
 const pagination = require('./BusinessLogic').pagination;
-const pagination2 = require('./BusinessLogic').pagination2;
 
-// test("Return 3 pages", () => {
-//     const pages = pagination(2, 3);
-//     expect(pages).toBe("1 2 3");
-// });
+describe('Pagination', () => {
+    it("Should return 3 pages", () => {
+        const pages = pagination(2, 3);
+        expect(pages).toBe("1 2 3");
+    });
+    
+    it("Should return 4 pages", () => {
+        const pages = pagination(1, 4);
+        expect(pages).toBe("1 2 3 4")
+    })
+    
+    it("Should return 5 pages (1-10) with *starting* elipses", () => {
+        const pages = pagination(8, 10);
+        expect(pages).toBe("1 ... 7 8 9 10")
+    })
 
-// test("For four pages", () => {
-//     const pages = pagination(1, 4);
-//     expect(pages).toBe("1 2 3 4")
-// })
+    it("Should return 5 pages (1-10) with *starting* elipses with current first page", () => {
+        const pages = pagination(1, 10);
+        expect(pages).toBe("1 2 ... 10")
+        
+    })
 
-// test("Should return pagination for 10 at pos 8", () => {
-//     const pages = pagination(8, 10);
-//     console.log(pages)
-//     expect(pages).toBe("1 ... 7 8 9 10")
-// })
+    it("Should return 5 pages (1-10) with *ending* elipses", () => {
+        const pages = pagination(3, 10);
+        expect(pages).toBe("1 2 3 4 ... 10")
+    })
+    
+    it("Should return 5 pages (1-10) with *ending* elipses with current last", () => {
+        const pages = pagination(10, 10);
+        expect(pages).toBe("1 ... 9 10")
+    })
 
-test("Return 3 pages", () => {
-    const pages = pagination2(2, 3);
-    expect(pages).toBe("1 2 3");
-});
-
-test("For four pages", () => {
-    const pages = pagination2(1, 4);
-    expect(pages).toBe("1 2 3 4")
-})
-
-test("Should return pagination2 for 10 at pos 8", () => {
-    const pages = pagination2(8, 10);
-    console.log(pages)
-    expect(pages).toBe("1 ... 7 8 9 10")
+    it("Should return 5 pages (1-10) with *both* elipses (1-10)", () => {
+        const pages = pagination(5, 10);
+        expect(pages).toBe("1 ... 4 5 6 ... 10")
+    })
 })
